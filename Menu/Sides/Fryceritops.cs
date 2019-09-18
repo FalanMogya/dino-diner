b8ef3f84cea5129e4dfafc2049d4ce7fb59d8a05
George Widenor <eaglewidenor@users.campus>
2019-09-18 15:27:11 -0500

    Add Fryceritops

--- Menu/Sides/Fryceritops.cs
+++ Menu/Sides/Fryceritops.cs
@@ -4,7 +4,48 @@ using System.Text;
 
 namespace DinoDiner.Menu.Sides
 {
-    public class Fryceritops
+    public class Fryceritops : Side
     {
+        public override List<string> Ingredients
+        {
+            get
+            {
+                return new List<string>() { "Potato", "Salt", "Vegetable Oil" };
+            }
+        }
+
+        private Size size;
+
+        public override Size Size
+        {
+            set
+            {
+                size = value;
+                switch (size)
+                {
+                    case Size.Large:
+                        Price = 1.95;
+                        Calories = 480;
+                        break;
+                    case Size.Medium:
+                        break;
+                    case Size.Small:
+                        break;
+                }
+            }
+
+            get
+            {
+                return size;
+            }
+        }
+
+        public Fryceritops()
+        {
+            this.Price = 0.99;
+            this.Calories = 222;
+        }
+        
+
     }
 }
